0b235ee49eff0aacb085efba2a6bb536928a4ebd
songzhengpei <1121950253@qq.com>
2026-07-03 16:02:05 +0800

    fix: add missing GlobalState import in CommonService

--- android/service/src/main/java/com/follow/clash/service/CommonService.kt
+++ android/service/src/main/java/com/follow/clash/service/CommonService.kt
@@ -4,6 +4,7 @@ import android.app.Service
 import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
+import com.follow.clash.common.GlobalState
 import com.follow.clash.core.Core
 import com.follow.clash.service.modules.NetworkObserveModule
 import com.follow.clash.service.modules.NotificationModule
